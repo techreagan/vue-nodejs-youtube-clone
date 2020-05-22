a20d5fe78082e194b74902ae9b756d7529a1c046
Tech Reagan <reaganekhameye@gmail.com>
2020-05-22 14:22:08 +0100

    Add trending

--- src/views/Trending.vue
+++ src/views/Trending.vue
@@ -7,7 +7,7 @@
           sm="7"
           md="10"
           lg="10"
-          v-for="i in 10"
+          v-for="(video, i) in loading ? 12 : videos"
           :key="i"
           class="mx-lg-0 mx-md-0 mx-sm-auto mx-auto"
         >
@@ -19,13 +19,14 @@
             tile
             large
           >
-            <v-card class="card" tile flat router to="/watch/dd">
+            <v-card class="card" tile flat :to="`/watch/${video._id}`">
               <v-row no-gutters>
                 <v-col class="mx-auto" cols="12" sm="8" md="5" lg="4">
                   <!-- <v-responsive max-height="100%"> -->
                   <v-img
+                    max-height="200"
                     class="align-center"
-                    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                    :src="`${getUrl}/uploads/thumbnails/${video.thumbnailUrl}`"
                   >
                   </v-img>
                   <!-- </v-responsive> -->
@@ -33,20 +34,17 @@
                 <v-col class="hidden-sm-and-down">
                   <div class="ml-4 ">
                     <v-card-title class="pl-2 pt-0 subtitle-1 font-weight-bold">
-                      Top western road trips
+                      {{ video.title }}
                     </v-card-title>
 
-                    <v-card-subtitle class="pl-2 pb-0">
-                      1,000 miles of wonder
-                      <v-icon>mdi-circle-small</v-icon>9.6k
-                      views<v-icon>mdi-circle-small</v-icon>6 hours ago
+                    <v-card-subtitle class="pl-2 pb-0" v-if="video.userId">
+                      {{ video.userId.channelName }}
+                      <v-icon>mdi-circle-small</v-icon
+                      >{{ video.views }} views<v-icon>mdi-circle-small</v-icon
+                      >{{ dateFormatter(video.createdAt) }}
                     </v-card-subtitle>
                     <v-card-subtitle class="pl-2 pt-0">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Molestiae exercitationem minus in asperiores. Dolorem
-                      dicta ea praesentium, repellendus ipsam harum optio,
-                      delectus facilis ratione quam quaerat a architecto hic
-                      reprehenderit.
+                      {{ truncateText(video.description, 200) }}
                     </v-card-subtitle>
                   </div>
                 </v-col>
@@ -55,21 +53,102 @@
           </v-skeleton-loader>
           <!-- </v-container> -->
         </v-col>
+        <v-col cols="12" sm="12" md="12" lg="12">
+          <infinite-loading @infinite="getVideos">
+            <div slot="spinner">
+              <v-progress-circular
+                indeterminate
+                color="red"
+              ></v-progress-circular>
+            </div>
+            <div slot="no-results"></div>
+            <span slot="no-more"></span>
+            <div slot="error" slot-scope="{ trigger }">
+              <v-alert prominent type="error">
+                <v-row align="center">
+                  <v-col class="grow">
+                    <div class="title">Error!</div>
+                    <div>
+                      Something went wrong, but don’t fret — let’s give it
+                      another shot.
+                    </div>
+                  </v-col>
+                  <v-col class="shrink">
+                    <v-btn @click="trigger">Take action</v-btn>
+                  </v-col>
+                </v-row>
+              </v-alert>
+            </div>
+          </infinite-loading>
+        </v-col>
       </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
+import InfiniteLoading from 'vue-infinite-loading'
+import moment from 'moment'
+import { mapGetters } from 'vuex'
+
+import VideoService from '@/services/VideoService'
+
 export default {
-  name: 'Home',
+  name: 'Trending',
   data: () => ({
-    loading: true
+    loading: false,
+    loaded: false,
+    errored: false,
+    videos: [],
+    page: 1
   }),
-  mounted() {
-    setTimeout(() => {
-      this.loading = false
-    }, 3000)
+  computed: {
+    ...mapGetters(['currentUser', 'getUrl', 'isAuthenticated'])
+  },
+  methods: {
+    async getVideos($state) {
+      if (!this.loaded) {
+        this.loading = true
+      }
+
+      const videos = await VideoService.getAll('public', {
+        page: this.page,
+        sort: 'views'
+      })
+        .catch((err) => {
+          console.log(err)
+          this.errored = true
+        })
+        .finally(() => {
+          this.loading = false
+        })
+
+      if (typeof videos === 'undefined') return
+
+      if (videos.data.data.length) {
+        this.page += 1
+
+        this.videos.push(...videos.data.data)
+
+        $state.loaded()
+
+        this.loaded = true
+      } else {
+        $state.complete()
+      }
+    },
+    truncateText(string = '', num) {
+      if (string.length <= num) {
+        return string
+      }
+      return string.slice(0, num)
+    },
+    dateFormatter(date) {
+      return moment(date).fromNow()
+    }
+  },
+  components: {
+    InfiniteLoading
   }
 }
 </script>
